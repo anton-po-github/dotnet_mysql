5c67127fa018e4fa01c4ad3e972a1ec91acae535
AntonPo <myworkinggemail@gmail.com>
2025-02-18 09:13:10 +0200

    init

---
+++ Models/Users/UpdateRequest.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Users;
+
+public class UpdateRequest
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+
+
+    private string replaceEmptyWithNull(string value)
+    {
+        return string.IsNullOrEmpty(value) ? null : value;
+    }
+}
